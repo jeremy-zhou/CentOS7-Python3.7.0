f4d8a6d34d872473fe631f152bb8bba04ebe9842
jeremy_zhou <feng4206yu@163.com>
2020-09-01 17:04:13 +0800

    init commit

---
+++ CentOS7-Python3.7.0.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+yum install -y wget gcc gcc-c++ openssl-devel bzip2-devel libffi-devel
+
+wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tgz
+tar xzvf Python-3.7.0.tgz
+cd Python-3.7.0
+./configure --enable-optimizations
+make -j4 altinstall
+
+mv /usr/bin/python /usr/bin/python.bak
+ln -s /usr/local/bin/python3.7 /usr/bin/python
+ln -s /usr/local/bin/pip3.7 /usr/bin/pip
+
+python -V
+pip -V
